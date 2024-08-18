04c9d2f8b90b2eda5b6706f284af6f7672c2edea
mukundbondre <msbondre2001@gmail.com>
2024-08-19 02:28:50 +0530

    completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Popup from './Popup';
+import axios from 'axios';
 
 function App() {
 
-  const [popup, setPopup] = useState(false)
+  const [popup, setPopup] = useState(false);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/cards").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, [popup])
+
   const handelClick = (e) => {
     e.preventDefault();
     setPopup(true)
   }
+
+  const row = data.filter((item) => {
+    return search.toLowerCase() === '' ? item : item.title.toLowerCase().includes(search);
+  }).map((e) => {
+    return (
+      <tr key={e.id}>
+        <td>{e.title}</td>
+        <td>{e.description}</td>
+        <td>{e.link}</td>
+      </tr>
+    )
+  })
   return (
     <div className='container'>
-      <button onClick={handelClick} className='addData'>add data</button>
+      <div className='search'>
+        <input type='text'
+          placeholder='Search Title'
+          onChange={(e) => { setSearch(e.target.value) }}
+        />
+        <button onClick={handelClick} className='addBtn'>Add Data</button>
+      </div>
       <div className='table'>
         <table>
           <thead>
@@ -22,15 +53,11 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>title1</td>
-              <td>description1</td>
-              <td>link1</td>
-            </tr>
+            {row}
           </tbody>
         </table>
       </div>
-      {popup?<Popup setPopup={setPopup}/>:null}
+      {popup ? <Popup setPopup={setPopup} /> : null}
     </div>
   );
 }
--- frontend/src/Popup.js
+++ frontend/src/Popup.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
+import validator from 'validator'
 
-import './App.css';
+import './popup.css';
+import axios from 'axios';
 
 function Popup({ setPopup }) {
     const [data, setData] = useState({});
 
     const handelSubmit = (e) => {
         e.preventDefault();
-        console.log(data);
-        setPopup(false)
+
+        if (validator.isURL(data.link)) {
+            axios.post("http://localhost:8080/cards", data).then((res) => {
+                console.log(res.data);
+            }).catch((error) => {
+                console.log(error);
+            })
+
+            setPopup(false);
+        } else {
+            alert("Invalid URL");
+        }
     }
     return (
         <div className='popup'>
@@ -18,18 +30,21 @@ function Popup({ setPopup }) {
                     placeholder='Title'
                     value={data.title}
                     onChange={(e) => { setData({ ...data, title: e.target.value }) }}
+                    required
                 />
                 <input type='text'
                     placeholder='Description'
                     value={data.description}
                     onChange={(e) => { setData({ ...data, description: e.target.value }) }}
+                    required
                 />
-                <input type='text'
+                <input type='link'
                     placeholder='Link'
                     value={data.link}
                     onChange={(e) => { setData({ ...data, link: e.target.value }) }}
+                    required
                 />
-                <button type='submit'>Submit</button>
+                <button type='submit' className='submitBtn'>Submit</button>
             </form>
         </div>
     )
